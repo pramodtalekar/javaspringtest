62055b5bd16570d07871f8a771f45d8360304ab1
Pramod <pramodonjava@gmail.com>
2025-04-02 20:11:24 +0530

    test commit 9

--- src/main/java/ttl/JavaHelloWorldApplication.java
+++ src/main/java/ttl/JavaHelloWorldApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaHelloWorldApplication {
     public static void main(String[] args) {
 
-        System.out.println("Test....");
+        System.out.println("Testing....");
 
         SpringApplication.run(JavaHelloWorldApplication.class, args);
     }
